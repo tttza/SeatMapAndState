35850d2205533053f59e24cfbb4cfe080a7a199c
tttza <25410554+tttza@users.noreply.github.com>
2020-11-25 19:22:21 +0900

    画像表示修正

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -33,19 +33,7 @@ interface NavBarState {
 }
 
 function UserAvatar(props: any) {
-  // If a user avatar is available, return an img tag with the pic
-  // if (props.user.avatar) {
-  //   return <img
-  //     src={props.user.avatar} alt="user"
-  //     className="rounded-circle align-self-center mr-2"
-  //     style={{ width: '32px' }}></img>;
-  // }
-
-  return (<Person person-query="me" />)
-  // No avatar available, return a default icon
-  return <i
-    className="far fa-user-circle fa-lg rounded-circle align-self-center mr-2"
-    style={{ width: '32px' }}></i>;
+  return (<Person personQuery="me" />)
 }
 
 function AuthNavItem(props: NavBarProps) {
